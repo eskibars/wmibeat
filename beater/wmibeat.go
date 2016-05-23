bf289e54961c9a6cf5cf73315ae6148df985911c
Artsiom Baranovskiy <Artsiom_Baranovskiy@epam.com>
2016-05-23 13:45:36 +0300

    Update wmibeat to save WMI result object as separate docs
    - The aim is to provide better filtering/searching abilities on Kibana
    - Remove redundant code including ObjectTitle column

--- beater/wmibeat.go
+++ beater/wmibeat.go
@@ -5,7 +5,6 @@ import (
 	"time"
 	"strings"
 	"bytes"
-	"strconv"
 
 	"github.com/elastic/beats/libbeat/beat"
 	"github.com/elastic/beats/libbeat/cfgfile"
@@ -111,102 +110,76 @@ func (bt *Wmibeat) RunOnce(b *beat.Beat) error {
 
 	service := serviceObj.ToIDispatch()
 
-	var allValues common.MapStr
 	for _, class := range bt.beatConfig.Wmibeat.Classes {
-		if len(class.Fields) > 0 {
-			var query bytes.Buffer
-			wmiFields := class.Fields
-			query.WriteString("SELECT ")
-			query.WriteString(strings.Join(wmiFields, ","))
-			query.WriteString(" FROM ")
-			query.WriteString(class.Class)
-			if class.WhereClause != "" {
-				query.WriteString(" WHERE ")
-				query.WriteString(class.WhereClause)
-			}
-			logp.Info("Query: " + query.String())
-
-			resultObj, err := oleutil.CallMethod(service, "ExecQuery", query.String(), "WQL")
-			if err != nil {
-				logp.Err("Unable to execute query: %v", err)
-				return err
-			}
-			defer resultObj.Clear()
-
-			result := resultObj.ToIDispatch()
-			countObj, err := oleutil.GetProperty(result, "Count")
-			if err != nil {
-				logp.Err("Unable to get result count: %v", err)
-				return err
-			}
-			defer countObj.Clear()
-
-			count := int(countObj.Val)
-
-			var classValues interface {} = nil
-			
-			if (class.ObjectTitle != "") {
-				classValues = common.MapStr{}
-			} else {
-				classValues = []common.MapStr{}
-			}
-			for i :=0; i < count; i++ {
-				rowObj, err := oleutil.CallMethod(result, "ItemIndex", i)
-				if err != nil {
-					logp.Err("Unable to get result item by index: %v", err)
-					return err
-				}
-				defer rowObj.Clear()
-
-				row := rowObj.ToIDispatch()
-				var rowValues common.MapStr
-				var objectTitle = ""
-				for _, j := range wmiFields {
-					wmiObj, err := oleutil.GetProperty(row, j)
-					if err != nil {
-						logp.Err("Unable to get propery by name: %v", err)
-						return err
-					}
-					defer wmiObj.Clear()
-
-					var objValue = wmiObj.Value()
-					if (class.ObjectTitle == j) {
-						objectTitle = objValue.(string)
-					}
-					rowValues = common.MapStrUnion(rowValues, common.MapStr { j: objValue } )
-					
-				}
-				if (class.ObjectTitle != "") {
-					if (objectTitle != "") {
-						classValues =  common.MapStrUnion(classValues.(common.MapStr), common.MapStr { objectTitle: rowValues })
-					} else {
-						classValues =  common.MapStrUnion(classValues.(common.MapStr), common.MapStr { strconv.Itoa(i): rowValues })
-					}
-				} else {
-					classValues = append(classValues.([]common.MapStr), rowValues)
-				}
-				rowValues = nil
-			}
-			allValues = common.MapStrUnion(allValues, common.MapStr { class.Class: classValues })
-			classValues = nil
-			
-		} else {
+		if len(class.Fields) == 0 {
 			var errorString bytes.Buffer
 			errorString.WriteString("No fields defined for class ")
 			errorString.WriteString(class.Class)
 			errorString.WriteString(".  Skipping")
 			logp.Warn(errorString.String())
+			continue
+		}
+
+		var query bytes.Buffer
+		wmiFields := class.Fields
+		query.WriteString("SELECT ")
+		query.WriteString(strings.Join(wmiFields, ","))
+		query.WriteString(" FROM ")
+		query.WriteString(class.Class)
+		if class.WhereClause != "" {
+			query.WriteString(" WHERE ")
+			query.WriteString(class.WhereClause)
+		}
+		logp.Info("Query: " + query.String())
+
+		resultObj, err := oleutil.CallMethod(service, "ExecQuery", query.String(), "WQL")
+		if err != nil {
+			logp.Err("Unable to execute query: %v", err)
+			return err
+		}
+		defer resultObj.Clear()
+
+		result := resultObj.ToIDispatch()
+		countObj, err := oleutil.GetProperty(result, "Count")
+		if err != nil {
+			logp.Err("Unable to get result count: %v", err)
+			return err
+		}
+		defer countObj.Clear()
+
+		count := int(countObj.Val)
+
+		for i :=0; i < count; i++ {
+			rowObj, err := oleutil.CallMethod(result, "ItemIndex", i)
+			if err != nil {
+				logp.Err("Unable to get result item by index: %v", err)
+				return err
+			}
+			defer rowObj.Clear()
+
+			row := rowObj.ToIDispatch()
+
+			event := common.MapStr{
+				"@timestamp": common.Time(time.Now()),
+				"type":       b.Name,
+				"class":    class.Class,
+			}
+
+			for _, fieldName := range wmiFields {
+				wmiObj, err := oleutil.GetProperty(row, fieldName)
+				if err != nil {
+					logp.Err("Unable to get propery by name: %v", err)
+					return err
+				}
+				defer wmiObj.Clear()
+
+				var objValue = wmiObj.Value()
+				event[fieldName] = objValue
+			}
+
+			b.Events.PublishEvent(event)
 		}
 	}
-
-	event := common.MapStr{
-		"@timestamp": common.Time(time.Now()),
-		"type":       b.Name,
-		"wmi":    allValues,
-	}
-
-	b.Events.PublishEvent(event)
-	logp.Info("Event sent")
 
 	return err
 }
